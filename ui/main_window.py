a7d64a0f0219a4a92bc44915cfd9e4254c0a3f3c
Sobottasgithub <Sobotta2008@outlook.de>
2024-02-10 19:05:55 +0100

    Add delete emails
    - deleteNow       -> on button press
    - deleteAutomatic -> after one hour

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,6 +1,7 @@
-import json, datetime, imaplib, functools
-from PyQt5 import QtWidgets, uic
+import json, datetime, imaplib, functools, ssl
+from PyQt5 import QtWidgets, uic, QtCore
 from utils import paths
+from ui.utils import helpers
 
 import logging
 logger = logging.getLogger(__name__)
@@ -12,17 +13,22 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.data = []
 
-        self.createNewEmail()
+        if len(self.data) == 0:
+            self.createNewEmail()
         self.uiButton_add.clicked.connect(self.createNewEmail)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.deleteEmailsAllAccounts)
+        self.timer.start(36000000)
     
     def createNewEmail(self):
         formLayout, uiItems = self.createEmailForm()
 
-        #put it in a widget
+        # Put QForm in a widget
         formWidget = QtWidgets.QWidget()
         formWidget.setLayout(formLayout)
 
-        #put it in a qlistwidget item
+        # Put widget in a qlistwidget item
         listWidgetItem = QtWidgets.QListWidgetItem()
         listWidgetItem.setSizeHint(formWidget.sizeHint())
 
@@ -32,7 +38,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.data.append({"uiItems": uiItems})
         uiItems["deleteNow"].clicked.connect(functools.partial(self.deleteNow, uiItems))
-        uiItems["deleteEmail"].clicked.connect(functools.partial(self.deleteEmail, {"uiItems": uiItems}, listWidgetItem))
+        uiItems["deleteEmail"].clicked.connect(functools.partial(self.deleteEmailAccount, {"uiItems": uiItems}, listWidgetItem))
 
     def createEmailForm(self):
         uiLineEdit_email        = QtWidgets.QLineEdit()
@@ -46,7 +52,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         uiSpinBox_port.setMaximum(99999)
         uiSpinBox_port.setValue(933)
-        uiCombobox_expiryDate.addItems(["1 Day", "4 Days", "1 Week", "2 Weeks" "1 Month", "4 Month", "6 Month", "1 Year"])
+        uiCombobox_expiryDate.addItems(["1 Day", "4 Days", "1 Week", "2 Weeks", "1 Month", "4 Months", "6 Months", "1 Year"])
 
         # Add items to QForm
         formLayout = QtWidgets.QFormLayout()
@@ -71,8 +77,47 @@ class MainWindow(QtWidgets.QMainWindow):
         return (formLayout, uiItems)
 
     def deleteNow(self, uiItems):
-        pass
+        self.deleteEmails(helpers.uiItemsToValues(uiItems))
 
-    def deleteEmail(self, items, listWidgetItem):
+    def deleteEmailAccount(self, items, listWidgetItem):
         listWidgetItem.setHidden(True)
         del self.data[self.data.index(items)]
+
+    def deleteEmailsAllAccounts(self):
+        if len(self.data) != 0:
+            for emailAccount in self.data:
+                self.deleteEmails(helpers.uiItemsToValues(emailAccount["uiItems"]))
+
+    def deleteEmails(self, emailValues):
+        # Check if all credentials were entered
+        for data in emailValues.values():
+            if data == "":
+                helpers.createMessageWindow(self, "Not all credentials were entered")
+                return
+        try:
+            if emailValues["startTls"]:
+                tls_context = ssl.create_default_context()
+            
+            # Connect to server
+            serverConnection = imaplib.IMAP4_SSL(emailValues["emailServer"], emailValues["port"])
+            if emailValues["startTls"]:
+                serverConnection.starttls(ssl_context=tls_context)
+            serverConnection.login(emailValues["email"], emailValues["password"])
+
+            # Search for emails
+            serverConnection.select('Inbox')
+            beforeDate = (datetime.date.today() - datetime.timedelta(emailValues["expiryDate"])).strftime("%d-%b-%Y")
+
+            resp, data = serverConnection.uid('search',None, '(BEFORE {0})'.format(beforeDate)) # search and return Uids
+            uids = data[0].split()    
+            for uid in uids:
+                resp,data = serverConnection.uid('fetch',uid,"(BODY[HEADER])")             
+                serverConnection.uid('STORE',uid, '+FLAGS', '(\\Deleted)')
+            serverConnection.expunge()
+                
+            serverConnection.close()
+            serverConnection.logout()
+
+        except Exception as error:
+            helpers.createMessageWindow(self, error)
+            return
--- ui/utils/helpers.py
+++ ui/utils/helpers.py
@@ -8,3 +8,17 @@ def createMessageWindow(window, message):
         QtWidgets.QMessageBox.Ok
     )
     return msgBox
+
+def uiItemsToValues(uiItems):
+    data = {}
+    data["emailServer"] =  uiItems["emailServer"].text()
+    data["port"] = uiItems["port"].value()
+    data["email"] = uiItems["email"].text()
+    data["password"] = uiItems["password"].text()
+    data["expiryDate"] = dateStringToDate(uiItems["expiryDate"].currentText())
+    data["startTls"] = uiItems["startTls"].isChecked()
+    return data
+
+def dateStringToDate(dateString):
+    dateStrings = {"1 Day": 1, "4 Days": 4, "1 Week": 7, "2 Weeks": 14, "1 Month": 31, "4 Months": 122, "6 Months": 183, "1 Year": 365}
+    return dateStrings[dateString]
